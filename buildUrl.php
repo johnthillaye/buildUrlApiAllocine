b9f686bb55b35c637d34bf03611097464c437977
John <johnthillaye@gmail.com>
2013-04-21 18:12:44 +0200

    added examples

--- buildUrl.php
+++ buildUrl.php
@@ -1,5 +1,5 @@
 <?php
-	//build url vers api allocine
+	//build url to use the allocine API
 	
 	function buildUrlApiAllocine ($route, $tokens) {
 		
@@ -12,14 +12,24 @@
 		
 		$tokensUrl = implode("&", $tokens);
 		
+		//Generate signature
 		$sig = rawurlencode(base64_encode(sha1('e2b7fd293906435aa5dac4be670e7982'.$tokensUrl.'&sed='.$sed, true)));
 		
 		return 'http://api.allocine.fr/rest/v3/' . $route . '?' . $tokensUrl . "&sed=" . $sed . "&sig=" . $sig;
 	}
 	
-	//EXAMPLE 1
+	//EXAMPLE 1 - search
+	$searchTokens[] = "q=avatar";
+	$searchTokens[] = "filter=movie";
+	$searchTokens[] = "count=50";
+	$searchTokens[] = "page=1";
 	
-	$tokens[] = "code=37607";
+	$url = buildUrlApiAllocine("search", $searchTokens);
 	
-	$url = buildUrlApiAllocine("movie", $tokens);
+	//EXAMPLE 2 - movie data
+	
+	$movieTokens[] = "code=37607";
+	
+	$url = buildUrlApiAllocine("movie", $movieTokens);
+	
 ?>
